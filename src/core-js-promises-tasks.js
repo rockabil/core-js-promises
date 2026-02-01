2f738613bc785a861d4cbdeb9444eec773d98b37
rockabil <rockabil81@gmail.com>
2026-02-01 23:32:32 +0300

    feat: add implementation of getFirstResolvedPromiseResult

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -56,8 +56,24 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    let rejectedCount = 0;
+
+    promises.forEach((promise) => {
+      promise
+        .then((value) => {
+          resolve(value);
+        })
+        .catch(() => {
+          rejectedCount += rejectedCount;
+
+          if (rejectedCount === promises.length) {
+            reject();
+          }
+        });
+    });
+  });
 }
 
 /**
